a4ee50bed16ac10bc307cddb6564d9b041332ece
DevTest <115026138+adrien-pago@users.noreply.github.com>
2024-02-27 11:17:06 +0100

    fichier xlsx

--- PHP/list-files.php
+++ PHP/list-files.php
@@ -4,9 +4,10 @@ $files = array_diff(scandir($directory), array('..', '.'));
 
 $result = [];
 foreach ($files as $file) {
-    if (preg_match('/\.(pdf|docx?)$/i', $file)) {
+    if (preg_match('/\.(pdf|docx?|xlsx)$/i', $file)) {
         $result[] = $file;
     }
 }
 
 echo json_encode($result);
+?>
